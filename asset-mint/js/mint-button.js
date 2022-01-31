b0f5c4126f3fd71c051cde65226f382f629754fa
github_username <you@domain.com>
2022-01-31 00:14:50 -0500

    button added

--- asset-mint/js/mint-button.js
+++ asset-mint/js/mint-button.js
@@ -1,8 +1,9 @@
-  var counterVal = 0;
+var counterVal = 0;
 
 function incrementClick() {
   if (counterVal >=0){
     updateDisplay(++counterVal);
+    updateprice(counterVal*0.06)
   }
 }
 
@@ -14,9 +15,14 @@ function resetCounter() {
 function decrementClick(){
   if (counterVal >=1){
     updateDisplay(--counterVal);
+    updateprice(counterVal*0.06)
   }
 }
 
 function updateDisplay(val) {
     document.getElementById("counter-label").innerHTML = val;
+}
+
+function updateprice(val){
+    document.getElementById("total-price").innerHTML = val;
 }
